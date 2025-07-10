9bcd7deb347722e567765f1e060dc4404a086e8c
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-10 22:15:00 +0800

    type converrsion, int.Parse, int.TryParse

---
+++ HomeWorkTypeConversions/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+// capture user age, 
+// how old the ywill be in 25 years
+// how old they are 25 years ago
+// print
+
+Console.Write("Age:");
+string? ageText = Console.ReadLine();
+//int? actualAge = int.Parse(age);
+
+bool isValidAge = int.TryParse(ageText, out int actualAge);
+
+// casting, tryparse
+Console.WriteLine($"You will be {actualAge + 25} in 25 years also your age 25 years ago was {actualAge - 25}");
---
+++ HomeWorkVariables/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+// variable, person's name, age, if they are alive, phone number.
+
+char name;
+int age;
+bool isAlive;
+string phoneNumber;
