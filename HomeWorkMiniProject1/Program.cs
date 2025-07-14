791bcf2b7c2ded39b3259723c29338bb946e458f
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-15 06:02:54 +0800

    reftractr - use return to make the code cleaner - return to the method and end the program?

--- HomeWorkMiniProject1/Program.cs
+++ HomeWorkMiniProject1/Program.cs
@@ -8,6 +8,20 @@ Console.Write("What is your name:");
 string? name = Console.ReadLine();
 
 
+
+
+Console.Write("What is your age:");
+string? ageText = Console.ReadLine();
+
+string formattedName;
+
+if(int.TryParse(ageText, out int age) == false)
+{
+    Console.WriteLine("Not a valid age");
+    return; //current method static void main
+    // exit out of the current method, when we return end the program.
+}
+
 if (name.ToLower() == "bob" || name.ToLower() == "sue")
 {
     Console.WriteLine($"Hello Professor {name}");
@@ -18,14 +32,19 @@ else
 }
 
 
-Console.Write("What is your age:");
-string? age = Console.ReadLine();
-
-bool validAge = int.TryParse(age, out int ageConverted);
-
-if (validAge && ageConverted < 21)
+if (age < 21)
 {
-    Console.WriteLine($"You should wait {21 - ageConverted} years to start this class");
+    Console.WriteLine($"You should wait {21 - age} years to start this class");
 }
+
+else
+{
+    Console.WriteLine("Welcome to class");
+}
+
+
+
+
+
 
 
